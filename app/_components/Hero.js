724ea79c3d132994ade2b25fdd678f6084e96cbd
Sharanu P <93566630+Sharanupatil8@users.noreply.github.com>
2026-03-26 21:36:21 +0530

    Update Hero.js

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -68,7 +68,7 @@ function Hero() {
       ref={heroRef}
       className="relative h-full px-2 md:px-4 w-full overflow-hidden "
     >
-      <div className="max-w-7xl h-full pb-16 z-40 mx-auto md:px-6 -mt-2 md:mt-0 lg:px-0">
+      <div className="max-w-7xl h-full pb-16 z-40 mx-auto md:px-6 -mt-8 md:mt-0 lg:px-0">
         <div className="w-full flex flex-col  text-center md:text-start justify-center h-full lg:w-4/5 lg:me-auto">
           <div ref={badgeRef} className="flex items-center md:justify-start gap-3 mb-8 -mt-4 md:mt-0">
             <span className="h-px w-7 bg-indigo-500 shrink-0" />
